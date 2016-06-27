e38aca630a90cfce63ef544181efff6660167d72
Peter Crossley <peter.crossley@webtrends.com>
2016-06-27 14:32:35 -0700

    adding style

---
+++ login.js
@@ -0,0 +1,14 @@
+/*
+Webtrends Login Javascript
+Copyright Webtrends, Inc. Webtrends authorized use only.
+*/
+
+/*
+Functions
+*/
+
+/*
+Items to run when the DOM is loaded
+*/
+document.addEventListener("DOMContentLoaded", function(event) {
+});
